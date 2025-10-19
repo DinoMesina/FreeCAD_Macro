2ea5b7823497cd1d89a6fcc46d07f1895a78bfdb
Dino del Favero <dino@mesina.net>
2025-10-20 00:58:41 +0200

    Add files via upload

--- BSplineParametric.py
+++ BSplineParametric.py
@@ -1,34 +1,6 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-"""
-***************************************************************************
-*   Copyright (c) 2018-2019 <Dino del Favero dino@delfavero.it>           *
-*                                                                         *
-*   This program is free software; you can redistribute it and/or modify  *
-*   it under the terms of the GNU Lesser General Public License (LGPL)    *
-*   as published by the Free Software Foundation; either version 3 of     *
-*   the License, or (at your option) any later version.                   *
-*   for detail see the LICENCE text file.                                 *
-*                                                                         *
-*   This software is distributed in the hope that it will be useful,      *
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-*   GNU Library General Public License for more details.                  *
-*                                                                         *
-*   You should have received a copy of the GNU Library General Public     *
-*   License along with this macro; if not, write to the Free Software     *
-*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
-*   USA                                                                   *
-***************************************************************************
-"""
-# Crea una Bspline discretizzando in maniera dinamica la geometria passata
-# 06/06/2019: v 0.0.1
-# 03/07/2019 Funziona anche con le sezioni di due superfici in FC0.18 (0.17no)
-
-__title__   = "BsplineParametric"
-__author__  = "Dino"
-__version__ = "0.1.0"
-__date__    = "03/01/2019"
+#/-*- coding: utf-8 -*-
+#
+# 06/06/19: v 0.0.1
 
 import FreeCAD, Part
 
